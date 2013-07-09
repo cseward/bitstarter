bdd36c6e8e6989738dce479cac7293f15310d524
Calvin Seward <meinhochbett@gmail.com>
2013-07-09 23:09:42 +0000

    made the thing read from file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
